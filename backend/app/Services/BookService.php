fa7577bcf259e954a47f1cd58e34831431aa7f6b
Tharindu Dhananjaya <herathhmtd.20@uom.lk>
2025-06-17 23:14:52 +0530

    Add BookService class to handle book-related operations

---
+++ backend/app/Services/BookService.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\BookRepository;
+
+class BookService
+{
+    protected $repo;
+
+    public function __construct(BookRepository $repo)
+    {
+        $this->repo = $repo;
+    }
+
+    public function getBooks($author, $is_available)
+    {
+        return $this->repo->getAll($author, $is_available);
+    }
+
+    public function getBook($id)
+    {
+        return $this->repo->getById($id);
+    }
+
+    public function createBook($data)
+    {
+        return $this->repo->create($data);
+    }
+
+    public function rentBook($id)
+    {
+        return $this->repo->rent($id);
+    }
+
+    public function returnBook($id)
+    {
+        return $this->repo->return($id);
+    }
+}
